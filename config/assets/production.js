2559cccda865bf8c0309f1e51536174381a0e989
Mark Wooff <mark.wooff@gmail.com>
2017-06-09 10:08:34 -0400

    Updated nav & added tooltip for Beta

--- config/assets/production.js
+++ config/assets/production.js
@@ -11,6 +11,7 @@ module.exports = {
         'public/lib/ng-img-crop/compile/minified/ng-img-crop.css',
         'public/lib/font-awesome/css/font-awesome.min.css',
         'public/lib/simple-line-icons/css/simple-line-icons.css',
+        'public/lib/tippy/tippy.css',
         // endbower
       ],
       js: [
@@ -29,6 +30,7 @@ module.exports = {
         'public/lib/ng-file-upload/ng-file-upload.min.js',
         'public/lib/ng-img-crop/compile/minified/ng-img-crop.js',
         'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+        'public/lib/tippy/tippy.min.js',
         // endbower
       ]
     },
